a4bfc73d6874bac0efc315dfcdbc11284d300950
Nayan Kurude <nkurude@atlassian.com>
2026-01-02 16:26:18 +0530

    jdklakdjla

--- src/controller/02-MyController.h
+++ src/controller/02-MyController.h
@@ -3,18 +3,18 @@
 
 #include "01-IMyController.h"
 
-RestController
-RequestMapping("/myUrlTee")
+/// @RestController
+/// @RequestMapping("/myUrlTee")
 /// @Component
 class MyController final : public IMyController {
   public:
 
-  GetMapping("/mysomeget2ee")
+  /// @GetMapping("/mysomeget2ee")
   RetDto myFunee(TestDto x) override{
     return RetDto();
   }
 
-  PostMapping("/somePost2ee")
+  /// @PostMapping("/somePost2ee")
   RetDto MyPostFunnee(TestDto x) override{
     println("MyPostFunnee called");
     println(x.a.value());
