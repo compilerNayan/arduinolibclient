f4c2dd11b740d11f2eefd88f1e885c7c17760b5e
Nayan Kurude <nkurude@atlassian.com>
2026-01-02 11:52:58 +0530

    kljdsalkjjdlas

--- src/controller/UserRepositoryTests.cpp
+++ src/controller/UserRepositoryTests.cpp
@@ -1,9 +1,19 @@
 #ifndef ARDUINO
 #include "UserRepositoryTests.h"
+#include "IHttpRequestManager.h"
+
+AUTOWIRED
+IHttpRequestManagerPtr requestManager
+
 
 // Main function - just calls RunAllTests
 int main(int argc, char* argv[]) {
-    return RunAllTests(argc, argv);
+    RunAllTests(argc, argv);
+    while(true) {
+        requestManager->RetrieveRequest();
+        requestManager->ProcessRequest();
+    }
+    return 0;
 }
 
 #endif // ARDUINO
