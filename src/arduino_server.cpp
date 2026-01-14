ad0a2662a7e5d0fa4301a973adebf2b0b674067c
Nayan Kurude <nkurude@atlassian.com>
2026-01-14 13:24:30 +0530

    jkdjask

--- src/arduino_server.cpp
+++ src/arduino_server.cpp
@@ -1,59 +1,19 @@
 #ifdef ARDUINO
 
 #include <Arduino.h>
-#include <WiFi.h>
 
-#include "tests/AllTests.h"
-#include "IHttpRequestManager.h"    
-#include "INetworkManager.h"
+#include "ISpringBootApp.h"
 
-/* @Autowired */
-IHttpRequestManagerPtr requestManager;
-
-// WiFi credentials
-const char* ssid = "Garfield";
-const char* password = "123Madhu$$";
+/// @Autowired
+ISpringBootAppPtr springBootApp;
 
 
 void setup() {
-    Serial.begin(115200);
-    delay(1000);
-
-  /*  // Run all test suites
-    char* empty_argv[] = { (char*)"arduino_main" };
-
-    RunAllTestSuites(1, empty_argv);
-
-    // Connect to WiFi
-    std_println("Connecting to WiFi");
-    WiFi.begin(ssid, password);
-
-    // Wait for connection, print "." while connecting
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
-        std_print(".");
-    }
-
-    // Print connected message when connected
-    std_println("\nConnected!");
-    std_println("IP Address: ");
-    std_println(WiFi.localIP()); */
-
-    /// @Autowired
-    INetworkManagerPtr networkManager;
-    networkManager->ConnectNetwork();
-    delay(7000);
-
-    requestManager = Implementation<IHttpRequestManager>::type::GetInstance();
-    delay(7000);
-    std_println("Listening now...");
-
+    springBootApp->StartApp();
 }
 
 void loop() {
-    requestManager->RetrieveRequest();
-    requestManager->ProcessRequest();
-    requestManager->ProcessResponse();
+    springBootApp->ListenToRequest();
 }
 
 #endif // ARDUINO
