27a54d10cfa726116d365ac876216bf03313ed75
Nayan Kurude <nkurude@atlassian.com>
2026-01-15 12:01:35 +0530

    kjdakjs

--- src/desktop_server.cpp
+++ src/desktop_server.cpp
@@ -1,21 +1,29 @@
 #ifndef ARDUINO
-#include "tests/AllTests.h"
-#include "IHttpRequestManager.h"
+//#include "tests/AllTests.h"
+//#include "IHttpRequestManager.h"
+
+#include "ISpringBootApp.h"
+
 
 /* @Autowired */
-IHttpRequestManagerPtr requestManager;
+ISpringBootAppPtr springBootApp;
 
 // Main function - runs the HTTP server loop
 int main(int argc, char* argv[]) {
 
-    RunAllTestSuites(argc, argv);
+   // RunAllTestSuites(argc, argv);
 
-
-    while(true) {
+    springBootApp->StartApp();
+/*    while(true) {
         requestManager->RetrieveRequest();
         requestManager->ProcessRequest();
         requestManager->ProcessResponse();
+    }*/
+
+    while(true) {
+        springBootApp->ListenToRequest();
     }
+
     return 0;
 }
 
