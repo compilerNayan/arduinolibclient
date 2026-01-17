cc93793aa9a9ae7e46259ac6e11fe9adf37ea48c
Nayan Kurude <nkurude@atlassian.com>
2026-01-17 16:23:15 +0530

    kjjdsakjdkjsa

--- src/controller/SwitchDto.h
+++ src/controller/SwitchDto.h
@@ -7,7 +7,16 @@
 /* @Serializable */
 class SwitchDto {
     Public optional<Int> id;
-    Public optional<SwitchState> switchState;
+    Public optional<StdString> switchState;
+
+    /**
+     * @brief Convert SwitchState enum to string representation
+     * @param state The switch state to convert
+     * @return "ON" if state is On, "OFF" if state is Off
+     */
+    Private Static inline StdString StateToString(SwitchState state) {
+        return (state == SwitchState::On) ? "ON" : "OFF";
+    }
 
     /**
      * @brief Default constructor
@@ -17,9 +26,9 @@ class SwitchDto {
     /**
      * @brief Parameterized constructor
      * @param id The switch ID
-     * @param switchState The switch state
+     * @param switchState The switch state enum
      */
-    Public SwitchDto(Int id, SwitchState switchState) : id(id), switchState(switchState) {}
+    Public SwitchDto(Int id, SwitchState switchState) : id(id), switchState(StateToString(switchState)) {}
 };
 
 #endif // SWITCHDTO_H
