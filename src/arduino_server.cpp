df27d8543cbd040b02c548ff81ae3aed6225648d
Nayan Kurude <nkurude@atlassian.com>
2026-02-04 11:44:24 +0530

    Revert "hjdsajhk"
    This reverts commit 7ca3b3a3a9eca776408d30b1188f82c075682084.

--- src/arduino_server.cpp
+++ src/arduino_server.cpp
@@ -3,23 +3,13 @@
 #include <Arduino.h>
 
 #include "IArduinoSpringBootApp.h"
-#include "tests/TestUtils.h"
-#include "thread_tests/ThreadPoolTests.h"
-#include "thread_tests/ThreadPoolMathExampleTests.h"
+#include "tests/AllTests.h"
 
 void setup() {
     Serial.begin(115200);
 
-    // Run only thread pool tests on Arduino (other suites need desktop/filesystem)
-    std_println("");
-    std_println("========================================");
-    std_println("  Arduino: Thread Pool Tests Only");
-    std_println("========================================");
-    RunAllThreadPoolTests();
-    RunAllThreadPoolMathExampleTests();
-    std_println("========================================");
-    std_println("  Thread pool tests done.");
-    std_println("========================================");
+    // Run all test suites (ThreadPoolTests, ThreadPoolMathExampleTests, etc.)
+    RunAllTestSuites(0, nullptr);
 
     /* @Autowired */
    // IArduinoSpringBootAppPtr springBootApp;
